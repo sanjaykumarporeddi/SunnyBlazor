0a5f47d6b659beea8ed7eb5231d0bd06b455bad4
sanjaykumarporeddi <156910608+sanjaykumarporeddi@users.noreply.github.com>
2025-04-18 22:37:23 +0100

    Toastr Notifications. Section 12

---
+++ Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.JSInterop;
+
+namespace SunnyBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+
+    }
+}
