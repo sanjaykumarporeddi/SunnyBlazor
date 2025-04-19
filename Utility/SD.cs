197b6dadf6176c694877d2b2ef5f2c2d42673a21
sanjaykumarporeddi <156910608+sanjaykumarporeddi@users.noreply.github.com>
2025-04-19 16:24:56 +0100

    not working

---
+++ Utility/SD.cs
@@ -0,0 +1,8 @@
+namespace SunnyBlazor.Utility
+{
+    public static class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+    }
+}
